4ce07faa0a2403b3e084d70c27bbb11aa21bb32b
MartaPniewska <kmpniewska@hotmail.com>
2024-09-20 17:55:28 +0200

    Add files via upload
    Dieses Programm schreibt eine Kurzgeschichte, in der Sie vorkommen. Folgende Angaben werden benötigt: Vorname, Monat, Haarfarbe, Wohnort.

---
+++ Geschichte.py
@@ -0,0 +1,21 @@
+# Geschichte.py
+
+# Einführung und Variable
+print("Das ist eine Geschichte, in der Sie vorkommen. Bitte geben Sie folgene Informationen an:")
+name = input("Ihr Vorname: ")
+bday = input("In welchem Monat ist Ihr Geburtstag: ")
+haarfarbe = input("Ihre Haarfarbe: ")
+wort = input("Ihr Wohnort:" )
+
+# Text
+print("Die Verabredung mit dem Kommissar")
+print()
+print("Es war ein grauer Morgen im " + bday +".")
+print("Die Sonne war gerade erst aufgegangen und es war noch wenig Betrieb im ")
+print("Stadtzentrum von " + wort + ".")
+print("Hauptkommissar Hartmann stand vor dem Bistro und schaute auf die Uhr. ")
+print("Wo bleibt " + name + "nur, dachte er. Ist etwas schief gelaufen?")
+print("Vielleicht hatte " + name + "s Freundin Wind von der Sache bekommen",
+      "und seine Pläne durchkreuzt.")
+print("Eine Person mit struwweligen " + haarfarbe )
+print(" + Haaren näherte sich mit raschen Schritten.")
